3a3c13724c6a4c3200d2157197a6233c806a3a4a
Mercytee <mercytngwenya@gmaill.com>
2023-06-09 12:28:28 +0200

    if statement

--- src/main/java/com/assignment_one/assignment_one/Model/AddNum.java
+++ src/main/java/com/assignment_one/assignment_one/Model/AddNum.java
@@ -3,16 +3,13 @@ package com.assignment_one.assignment_one.Model;
 public class AddNum implements Adding_One {
 
     public static void main(String[]args){
-        int n1= 5, n2= 6;
+        int n1= -5, n2= 6;
         int sum = n1 + n2;
+        if (n1 <0 || n2 <0){
+            System.out.println("Input number is negative");
+        }else{
         System.out.println(sum);
-    }
-
-        @Override
-        public int sum(int n1, int n2) {
-            return 0;
-        }
-    }
+    }}}
 
 
 

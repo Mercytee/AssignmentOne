50b36b29a730832a687cf4ac7fe6e07ddebb900f
Mercytee <mercytngwenya@gmaill.com>
2023-06-09 12:21:35 +0200

    modified code to check if either input is negative

--- src/main/java/com/assignment_one/assignment_one/Model/Addition.java
+++ src/main/java/com/assignment_one/assignment_one/Model/Addition.java
@@ -10,8 +10,15 @@ package com.assignment_one.assignment_one.Model;
     }
 
     @Override
-    public int sum(int n1, int n2) {
-        return n1+n2;
+    public int sum(int n1, int n2) throws IllegalArgumentException{
+        if (isNegative(n1,n2)){
+            throw new IllegalArgumentException("Input is negative");
+        }
+        return n1 + n2;
+    }
+
+    private boolean isNegative(int number,int number2){
+        return number<0||number2<0;
     }
 }
 

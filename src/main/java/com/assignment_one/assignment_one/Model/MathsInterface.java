87755bdd5c4445345db5c4a654b77f61fcf05620
Mercytee <mercytngwenya@gmaill.com>
2023-06-09 12:27:31 +0200

    added throw exception

--- src/main/java/com/assignment_one/assignment_one/Model/MathsInterface.java
+++ src/main/java/com/assignment_one/assignment_one/Model/MathsInterface.java
@@ -2,6 +2,6 @@ package com.assignment_one.assignment_one.Model;
 
 public interface MathsInterface {
    public  int sum(int n1, int n2, int n3, int n4);
-   public int sum(int n1, int n2);
+   public int sum(int n1, int n2) throws IllegalArgumentException;
 
 }
